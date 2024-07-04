b25acc6f42406a24c09b8859d76f6a05c7eda4a3
Dasha_Tsumbal <dasha.tsumbal21@gmail.com>
2024-07-04 19:38:39 +0200

    vita

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,30 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({
-  base: '/goit-js-hw-09/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'public/index.html'),
-        gallery: resolve(__dirname, 'public/1-gallery.html'),
-        form: resolve(__dirname, 'public/2-form.html'),
-      },
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-  },
-})
+    root: 'src',
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+      outDir: '../dist',
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
+});
